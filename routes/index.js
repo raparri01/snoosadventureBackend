57896df32bf26478ffdbbeb9bc5452beae9f4e5c
Rob <robertaparri@gmail.com>
2019-01-26 16:36:58 -0500

    Changed equipped array to an object with weapon and armor

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,10 @@ router.post('/create', function (req, res, next) {
         experienceNeeded: 2,
         gold: 0,
         inventory: [],
-        equipped: [],
+        equipped: {
+          weapon: "",
+          armor: ""
+        },
         createdOn: Date.now()
       });
       res.send({ message: `Welcome to the adventure, ${req.body.user}!` });
@@ -85,8 +88,8 @@ router.post('/battle', async function (req, res, next) {
   let monster = await monsterRef.get();
   monster = monster.data();
   let battleRecord = [];
-  let weaponMultiplier = 1.5; //TODO: Replace with weapon multiplier
-  let armorMultiplier = .87; // TODO: Replace with armor multiplier
+  let weaponMultiplier = user.equipped.weapon.length > 0 ? user.level * user.equipped.weapon.multiplier : 1.3;
+  let armorMultiplier = user.equipped.armor.length > 0? user.level * user.equipped.armor.multiplier : 1.0;
   while (monster.health > 0 && user.health > 0) {
     let userDamage = user.level * weaponMultiplier * (Math.random() * 3);
     let monsterDamage = monster.attack * (Math.random() * 3) * armorMultiplier;
@@ -97,15 +100,15 @@ router.post('/battle', async function (req, res, next) {
   }
   let win = monster.health > user.health ? false : true;
   let finalMessage;
-  if(win){
-    finalMessage = `You win! You have recieved ${monster.gold} gold and ${monster.experience} experience. You currently have ${user.experience + monster.experience} experience.`; 
+  if (win) {
+    finalMessage = `You win! You have recieved ${monster.gold} gold and ${monster.experience} experience. You currently have ${user.experience + monster.experience} experience.`;
     userRef.update({
       health: user.health,
       experience: user.experience + monster.experience
     });
   } else {
     finalMessage = `${monster.name} has defeated you!`;
-    if(user.experience - (user.experience * .1) < 0 ){
+    if (user.experience - (user.experience * .1) < 0) {
       userRef.update({
         health: 1,
         experience: 0
@@ -128,7 +131,7 @@ router.post('/levelUp', async function (req, res, next) {
   let userRef = db.collection('adventurers').doc(req.body.user);
   let user = await userRef.get();
   user = user.data();
-  if(user.experience >= user.experienceNeeded){
+  if (user.experience >= user.experienceNeeded) {
     userRef.update({
       level: user.level + 1,
       experience: user.experience - user.experienceNeeded,
